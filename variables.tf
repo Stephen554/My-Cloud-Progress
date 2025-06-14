621118b4cccdfa10fb9470422e1c5947281cc5a8
Stephen554 <67974133+Stephen554@users.noreply.github.com>
2025-06-14 15:52:24 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,1 @@
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "UK South"
-}
 
-variable "resource_group_name" {
-  description = "Resource Group Name"
-  type        = string
-}
