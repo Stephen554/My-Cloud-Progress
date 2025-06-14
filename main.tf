da2e2bfed7d89c9afd460a4d31c8946186b3092d
Stephen554 <67974133+Stephen554@users.noreply.github.com>
2025-06-14 15:50:20 +0100

    slight changes

--- main.tf
+++ main.tf
@@ -2,29 +2,29 @@ variable "prefix" {
   default = "tfvmex"
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "Prod_RG" {
   name     = "${var.prefix}-resources"
-  location = "West Europe"
+  location = "UK South"
 }
 
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.Prod_RG.location
+  resource_group_name = azurerm_resource_group.Prod_RG.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = azurerm_resource_group.Prod_RG.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.Prod_RG.location
+  resource_group_name = azurerm_resource_group.Prod_RG.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -35,8 +35,8 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.Prod_RG.location
+  resource_group_name   = azurerm_resource_group.Prod_RG.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
  }
@@ -69,12 +69,5 @@ resource "azurerm_virtual_machine" "main" {
   }
   tags = {
     environment = "staging"
-  }
-  terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorageacct"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
   }
 }
