032f7ab8e675e7d0fbcf331053802349d477e002
Stephen554 <67974133+Stephen554@users.noreply.github.com>
2025-06-14 15:23:41 +0100

    New changes to Azure

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "UK South"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+}
