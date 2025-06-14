95d8a6c6a54a845adafda63f06a8cf819ebcb043
Stephen554 <67974133+Stephen554@users.noreply.github.com>
2025-06-14 15:29:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,4 +69,11 @@ resource "azurerm_virtual_machine" "main" {
   tags = {
     environment = "staging"
   }
+  terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestorageacct"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
