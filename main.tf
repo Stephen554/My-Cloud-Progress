6f7f111ebaf25bdd008155ee4b04e7700fdd4c61
Stephen554 <67974133+Stephen554@users.noreply.github.com>
2025-06-25 12:25:08 +0100

    New VM

--- main.tf
+++ main.tf
@@ -43,18 +43,12 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "demo-vm"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
-
-  Uncomment this line to delete the OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
-
-  Uncomment this line to delete the data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -67,6 +61,7 @@ resource "azurerm_virtual_machine" "main" {
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
+    delete_os_disk_on_termination = true
   }
   os_profile {
     computer_name  = "hostname"
